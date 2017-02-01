dd51a607118e723e19b8ae59da59305e2b718af6
Alex Gladyshev <aleksey.gladyshev@gmail.com>
2017-02-01 17:13:18 -0500

    from scratch

---
+++ Alibaba/src/test/java/Homepage/TestHomePageSignIn.java
@@ -0,0 +1,17 @@
+package Homepage;
+
+import Base.CommonAPI;
+import org.testng.annotations.Test;
+
+/**
+ * Created by aleks_000 on 2/1/2017.
+ */
+public class TestHomePageSignIn extends CommonAPI {
+
+    @Test
+    public void AlibabaSignInIncorrectCredentials(){
+
+        //driver.findElement()
+
+    }
+}
---
+++ Generic/src/main/java/Base/CommonAPI.java
@@ -0,0 +1,35 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by aleks_000 on 1/30/2017.
+ */
+public class CommonAPI {
+
+    public WebDriver driver = null;
+
+    @Parameters ({"url"})
+    @BeforeMethod
+    public void setUp(String url) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\aleks_000\\IdeaProjects\\TestAppMentoring\\Generic\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(url);
+
+    }
+    @AfterMethod
+    public void tearDown(){
+
+        driver.close();
+        driver = null;
+    }
+}
