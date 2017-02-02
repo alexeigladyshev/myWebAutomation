4e8e64460f63e3c12e60f047aad2f2ace9cee4cb
Alex Gladyshev <aleksey.gladyshev@gmail.com>
2017-02-02 17:11:54 -0500

    Marginal updates

--- Alibaba/src/test/java/Homepage/TestHomePageSignIn.java
+++ Alibaba/src/test/java/Homepage/TestHomePageSignIn.java
@@ -1,6 +1,9 @@
 package Homepage;
 
 import Base.CommonAPI;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 /**
@@ -9,10 +12,15 @@ import org.testng.annotations.Test;
 public class TestHomePageSignIn extends CommonAPI {
 
     @Test
-    public void AlibabaSignInIncorrectCredentials(){
+    public void AlibabaSignInNavigate(){
 
-        //driver.findElement()
-        //on 54:04 of the video
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath(".//*[@id='J_SC_header']/header/div[2]/div[3]/div[1]/div[1]"))
+        ).moveToElement(driver.findElement(By.xpath(".//*[@id='J_SC_header']/header/div[2]/div[3]/div[1]/div[2]/div[1]/div[2]/a[1]"))
+        ).click().build().perform();
+        Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='fm-login-submit']")).isDisplayed());
+
+
 
     }
 }
