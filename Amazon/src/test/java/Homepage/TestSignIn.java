3839cd2f5454aacfc806a487d2243589fbebcb01
Alex Gladyshev <aleksey.gladyshev@gmail.com>
2017-02-04 16:48:06 -0500

    More marginal updates

--- Amazon/src/test/java/Homepage/TestSignIn.java
+++ Amazon/src/test/java/Homepage/TestSignIn.java
@@ -1,6 +1,8 @@
 package Homepage;
 
 import Base.CommonAPI;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
@@ -9,10 +11,34 @@ import org.testng.annotations.Test;
  */
 public class TestSignIn extends CommonAPI {
 
-    @Test
+    @Test(priority = 3)
     public void testSignInBlankCredentials(){
 
         Actions actions = new Actions(driver);
-        //actions.moveToElement()
+        actions.moveToElement(driver.findElement(By.xpath(".//*[@id='nav-link-accountList']/span[2]"))).moveToElement(driver.findElement(By.xpath(".//*[@id='nav-flyout-ya-signin']/a/span"))).build().perform();
+        clickByXpath(".//*[@id='signInSubmit']");
+        Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='auth-email-missing-alert']/div/div")).getText().contains("Enter your email or mobile phone number"));
+        Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='auth-password-missing-alert']/div/div")).getText().contains(" Enter your password "));
+    }
+    @Test(priority = 4)
+    public void TestSignInNoPassword(){
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath(".//*[@id='nav-link-accountList']/span[2]"))).moveToElement(driver.findElement(By.xpath(".//*[@id='nav-flyout-ya-signin']/a/span"))).build().perform();
+        typeByXpath(".//*[@id='ap_email']","Incorrectuser");
+        clickByXpath(".//*[@id='signInSubmit']");
+        org.testng.Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='auth-password-missing-alert']/div/div")).getText().contains("Enter your password"));
+    }
+
+    @Test(priority = 5)
+    public void TestSignInIncorrectCredentials(){
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath(".//*[@id='nav-link-accountList']/span[2]"))).moveToElement(driver.findElement(By.xpath(".//*[@id='nav-flyout-ya-signin']/a/span"))).build().perform();
+        typeByXpath(".//*[@id='ap_email']","Incorrectuser");
+        typeByXpath(".//*[@id='ap_password']", "Incorrectpassword");
+        clickByXpath(".//*[@id='signInSubmit']");
+        org.testng.Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='auth-warning-message-box']/div/div/ul/li/span")).getText().contains("To better protect your account"));
+
     }
 }
